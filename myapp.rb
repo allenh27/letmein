ec1c94009d67d29ef5b56bf3b374ad0e46747f07
allenh27 <allenh27@me.com>
2015-06-26 07:09:27 +1000

    Update myapp.rb

--- myapp.rb
+++ myapp.rb
@@ -7,5 +7,5 @@ get '/:number' do
     resp = RestClient.get "https://staging.api.telstra.com/v1/oauth/token?client_id=[client_id]&client_secret=[secret]&grant_type=client_credentials&scope=SMS"
     token = JSON.parse(resp)["access_token"]
     header =  {authorization: "Bearer #{token}", "Content-Type" => "application/json", "Accept" => "application/json"}
-    RestClient.post "https://staging.api.telstra.com/v1/sms/messages", {to: params[:number], body: "Hi Frank, we are at the front door. This has been sent using the Telstra API >:)"}.to_json, header
+    RestClient.post "https://staging.api.telstra.com/v1/sms/messages", {to: params[:number], body: "Hi Allen, we are at the front door. This has been sent using the Telstra API >:)"}.to_json, header
 end
